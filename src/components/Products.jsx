01a4f0c73ba32b2014197a92b76a041d6c9719c7
vinicius <viniciusarashiro0@gmail.com>
2024-02-12 11:42:31 -0300

    feat: add filter search

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,6 +3,8 @@ import { Product } from './Product'
 import { api } from '../provider'
 
 export const Products = () => {
+  const [search, setSearch] = useState('')
+
   const fetchData = async () => {
     try {
       const response = await api.get('/products')
@@ -18,22 +20,36 @@ export const Products = () => {
   useEffect(() => {
     fetchData()
   }, [])
-  console.log(products)
+
+  function handleSearch(event) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredProducts =
+    search !== ''
+      ? products.filter((product) =>
+          product.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : products
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <form className="w-full">
         <input
           type="text"
-          placeholder="Pesquisar um produto..."
+          placeholder="Pesquisar produto..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-950"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-4 gap-6 auto-rows-[250px]">
-        {products &&
-          products.map((item) => <Product key={item.id} item={item} />)}
+        {filteredProducts &&
+          filteredProducts.map((item) => <Product key={item.id} item={item} />)}
       </div>
     </div>
   )
