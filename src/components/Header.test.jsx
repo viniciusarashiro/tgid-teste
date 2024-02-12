96dfbf23a8974800c2bf3f482a55304babfba816
vinicius <viniciusarashiro0@gmail.com>
2024-02-12 17:29:32 -0300

    test: renders header with links

---
+++ src/components/Header.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import { Header } from './Header'
+import '@testing-library/jest-dom'
+
+test('renders header with links', () => {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  )
+
+  expect(screen.getByText('Produtos')).toBeInTheDocument()
+  expect(screen.getByText('Carrinho')).toBeInTheDocument()
+})
