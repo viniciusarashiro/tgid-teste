81c9b411f4d9eee14195b623eff94fcf04bb63e3
vinicius <viniciusarashiro0@gmail.com>
2024-02-12 17:48:06 -0300

    fix: test renders products search input

--- src/components/Header.test.jsx
+++ src/components/Header.test.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
-import { Header } from './Header'
+import { Products } from './Products'
 import '@testing-library/jest-dom'
 
 describe('Products', () => {
   test('renders products search input', () => {
     render(
       <BrowserRouter>
-        <Header />
+        <Products />
       </BrowserRouter>
     )
 
-    expect(screen.getByText('Produtos')).toBeInTheDocument()
-    expect(screen.getByText('Carrinho')).toBeInTheDocument()
+    expect(
+      screen.getByPlaceholderText('Pesquisar produto...')
+    ).toBeInTheDocument()
   })
 })
