a9e9b5375e85e51a3dd89d223f01b5a4f1b762fe
vinicius <viniciusarashiro0@gmail.com>
2024-02-12 13:21:46 -0300

    feat: add style to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <>
-      <nav>
-        <ul>
+      <nav className="bg-slate-600">
+        <ul className="flex flex-row justify-around p-4 align-middle">
           <li>
-            <a href="/">Produtos</a>
+            <Link to="/">Produtos</Link>
           </li>
           <li>
-            <a href="/cart">carrinho</a>
+            <Link to={'/cart'}>Carrinho</Link>
           </li>
         </ul>
       </nav>
